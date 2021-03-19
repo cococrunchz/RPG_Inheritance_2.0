e02ddb3e89117070baed395b64a3aca69e8274b2
BeeCabs <beecabido@gmail.com>
2021-03-19 11:25:00 +0800

    Initial commit

---
+++ app/src/main/java/mcm/edu/ph/rpg_inheritance_20/view/monster_menu.java
@@ -0,0 +1,16 @@
+package mcm.edu.ph.rpg_inheritance_20.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import mcm.edu.ph.rpg_inheritance_20.R;
+
+public class monster_menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_monster_menu);
+    }
+}
